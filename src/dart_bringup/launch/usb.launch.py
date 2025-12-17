4424a70985cd095a3663d4977ee46782abcc4480
FHb212 <33cc77dd66@gmail.com>
2025-12-17 15:49:13 +0800

    feat: add launch

---
+++ src/dart_bringup/launch/usb.launch.py
@@ -0,0 +1,21 @@
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('dart_transporter'),
+        'config',
+        'settings.yaml'
+    )
+
+    node = Node(
+        package='dart_transporter',
+        executable='usb_node',
+        name='usb_node',
+        output='both',
+        parameters=[config],
+    )
+
+    return LaunchDescription([node])
